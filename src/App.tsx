a9213b45e32e8acaa747f5ed8d3d6699543f0b18
Tsz Kin Wong <qq33553255@hotmail.com>
2023-01-26 01:15:34 +0800

    feat: add background to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 import { Menu } from './components/menu/Menu'
+import styled from 'styled-components'
+import bg2 from './assets/bg2.png'
+
+const SApp = styled.div<{src: string}>`
+    width: 100%;
+    height: 100vh;
+    background-image: url(${props => props.src});
+`
 
 const App = () => {
     const [isMenuOpen, useIsMenuOpen] = useState(true)
-    return <div>{isMenuOpen && <Menu />}</div>
+    return <SApp src={bg2}>{isMenuOpen && <Menu />}</SApp>
 }
 
 export default App
