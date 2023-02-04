5b79dc713f5c7de5ee961b7aefad47259bf9d113
princesteven2 <qq33553255@hotmail.com>
2023-02-04 15:56:58 +0800

    fix: refactor modal wrapper

--- packages/frontend/src/features/tutorial/TutorialContainer.tsx
+++ packages/frontend/src/features/tutorial/TutorialContainer.tsx
@@ -1,21 +1,15 @@
-import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import { isTutorialOpenedSelector } from '../../store/slice'
 import Tutorial from '../../components/tutorial/Tutorial'
+import { SModalWrapper } from '../../components/modal-wrapper/style'
 
-const STutorialWrapper = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-`
 const TutorialContainer = () => {
     const isOpened = useSelector(isTutorialOpenedSelector)
 
     return isOpened ? (
-        <STutorialWrapper>
+        <SModalWrapper>
             <Tutorial />
-        </STutorialWrapper>
+        </SModalWrapper>
     ) : (
         <></>
     )
